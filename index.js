f0538ac3e7da6db75be13b151b669b0fef9f386f
g_zawie <gzawie24@gmail.com>
2025-03-20 08:38:04 +0300

    DOM contentloaded

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+    // Select the paragraph element with id "text"
+    let paragraph = document.getElementById("text");
+  
+    // Update the text content
+    paragraph.textContent = "This is really cool!";
+  });
+  
